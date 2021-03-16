d7306c82f817ea4f155b213456851c8890aef02f
James Morgan <MorganJay@users.noreply.github.com>
2021-03-16 07:17:22 +0100

    Implemented Sql Repository and swapped out Mock Repo, Migrated Models into SQL Database
    Now getting data from the actual db

--- Commander/Startup.cs
+++ Commander/Startup.cs
@@ -1,19 +1,12 @@
 using Commander.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Commander
 {
@@ -43,9 +36,9 @@ namespace Commander
 
             services.AddCors(options => options.AddPolicy("AllowEverthing", builder => builder.AllowAnyOrigin()
                                                                                               .AllowAnyMethod()
-                                                                                              .AllowAnyHeader()));
-
-            services.AddScoped<ICommanderRepo, MockCommanderRepo>();
+                                                                                             .AllowAnyHeader()));
+            //services.AddScoped<ICommanderRepo, MockCommanderRepo>();
+            services.AddScoped<ICommanderRepo, SqlCommanderRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
